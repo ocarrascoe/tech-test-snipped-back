ea66f2aee41156522f65779dbed60adfaecd00b8
ocarrascoe <omar.carrascoe@gmail.com>
2022-05-20 16:38:00 +0200

    Add: User model
    Fix: Book model, add nullability

--- src/apps/book/models.py
+++ src/apps/book/models.py
@@ -6,13 +6,13 @@ from django.db import models
 
 class Book(models.Model):
     codigolibro = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=60, blank=False)
-    editorial = models.CharField(max_length=25, blank=True)
-    autor = models.CharField(max_length=40, blank=False)
-    genero = models.CharField(max_length=20, blank=False)
-    paisautor = models.CharField(max_length=20, blank=True)
-    numeropaginas = models.PositiveIntegerField(blank=False, validators=[MinValueValidator(1)])
-    anoedicion = models.CharField(max_length=5, blank=True)
-    precio = MoneyField(blank=False, max_digits=14, decimal_places=2, default_currency='EUR', validators=[
+    titulo = models.CharField(max_length=60, blank=False, null=False)
+    editorial = models.CharField(max_length=25, blank=True, null=True)
+    autor = models.CharField(max_length=40, blank=False, null=False)
+    genero = models.CharField(max_length=20, blank=False, null=False)
+    paisautor = models.CharField(max_length=20, blank=True, null=True)
+    numeropaginas = models.PositiveIntegerField(blank=False, null=False, validators=[MinValueValidator(1)])
+    anoedicion = models.CharField(max_length=5, blank=True, null=True)
+    precio = MoneyField(blank=False, null=False, max_digits=14, decimal_places=2, default_currency='EUR', validators=[
         MinMoneyValidator(1),
     ])
